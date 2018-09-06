132f615ef24e2761f1dbbd113c4911e1c902748a
Skaushal2 <43046786+Skaushal2@users.noreply.github.com>
2018-09-06 14:36:52 -0600

    Gridview

---
+++ src/Backend.Demo/BLL/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Demo.DAL;
+using Backend.Demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Demo.BLL
+{
+    [DataObject]
+    public class StudentController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Student> ListAllStudents() // the red squigel here probably means your student class is not public
+        {
+            using (var context = new MyDatabaseContext())
+            {
+                return context.Students.ToList();
+            }
+        }
+
+
+    }
+}
--- src/DemoWeb/About.aspx
+++ src/DemoWeb/About.aspx
@@ -4,4 +4,9 @@
     <h2><%: Title %>.</h2>
     <h3>Your application description page.</h3>
     <p>Use this area to provide additional information.</p>
+
+    <asp:GridView ID="StudentGridView" runat="server"></asp:GridView>
+
+    <asp:ObjectDataSource ID="StudentDataSource" runat="server" OldValuesParameterFormatString="original_{0}" SelectMethod="ListAllStudents" TypeName="Backend.Demo.BLL.StudentController"></asp:ObjectDataSource>
 </asp:Content>
+
